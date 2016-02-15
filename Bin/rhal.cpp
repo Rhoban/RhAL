212f3d1d49c6cae58f4b67e409ceb410efbf3a59
Gregwar <g.passault@gmail.com>
2016-02-15 16:58:12 +0100

    Bindings

--- Bin/rhal.cpp
+++ Bin/rhal.cpp
@@ -1,65 +1,24 @@
+#include <unistd.h>
 #include <vector>
 #include <iostream>
-#include <Bus/SerialBus.hpp>
-#include <Protocol/DynamixelV1.hpp>
-#include <tclap/CmdLine.h>
+#include <RhAL.hpp>
+#include <Bindings/RhioBinding.h>
 
 using namespace std;
 using namespace RhAL;
 
 int main(int argc, char *argv[])
 {
-    TCLAP::CmdLine cmd("RhAL command line tool", ' ', "0.1");
-    TCLAP::SwitchArg scan("S", "scan", "Scan for devices", cmd, false);
-    TCLAP::SwitchArg monitor("m", "monitor", "Monitors the devices", cmd, false);
-    TCLAP::ValueArg<std::string> ping("p", "ping", "Pings a device", false, "", "name|id", cmd);
-    TCLAP::ValueArg<std::string> dump("d", "dump", "Dumps registers of a device", false, "", "name|id", cmd);
-    TCLAP::ValueArg<std::string> blink("b", "blink", "Blinks a motor", false, "", "name|id", cmd);
-    TCLAP::ValueArg<std::string> set("r", "set", "Sets a register to a value", false, "", "name|id", cmd);
-    TCLAP::ValueArg<std::string> value("v", "value", "Value of the register", false, "", "name|id", cmd);
-    TCLAP::ValueArg<std::string> configure("c", "configure", "Configure a device", false, "", "name|id", cmd);
-    TCLAP::SwitchArg configureAll("C", "configure-all", "Configures all device", cmd, false);
-    TCLAP::SwitchArg save("s", "save", "Save the configuration", cmd, false);
-    cmd.parse(argc, argv);
-
-    if (scan.getValue()) {
-        // XXX: Scan
+    StandardManager manager;
+    // Default configuration
+    manager.setProtocolConfig("/dev/ttyACM0", 1000000, "DynamixelV1");
+    // Try to read config file
+    try {
+        manager.readConfig("rhal.json");
+    } catch (...) {
     }
+    RhioBinding binding(&manager);
+    binding.runScheduler();
 
-    if (monitor.getValue()) {
-        // XXX: Monitor
-    }
-
-    if (ping.getValue() != "") {
-        // XXX: Ping
-    }
-
-    if (dump.getValue() != "") {
-        // XXX: Dump
-    }
-
-    if (blink.getValue() != "") {
-        // XXX: Blink
-    }
-
-    if (set.getValue() != "") {
-        if (value.getValue() == "") {
-            std::cerr << "You should provide a value (with -v or --value)" << std::endl;
-        } else {
-            // XXX: Set a value
-        }
-    }
-
-    if (configure.getValue() != "") {
-        // XXX: Configure the servo
-    }
-
-    if (configureAll.getValue()) {
-        // XXX: Configure all servos
-    }
-
-    if (save.getValue()) {
-        std::cout << "Saving configuration..." << std::endl;
-        /// XXX: Save
-    }
+    while (1) sleep(1);
 }
--- Lib/Bindings/RhioBinding.cpp
+++ Lib/Bindings/RhioBinding.cpp
@@ -2,32 +2,68 @@
 #include <Devices/DXL.hpp>
 #include "RhioBinding.h"
 
-    RhioBinding::RhioBinding(Manager *manager_, std::string node_)
-: manager(manager_)
-{
-    // Getting RhIO node into node
-    RhIO::Root.newChild(node_);
-    node = *(RhIO::Root.child(node_));
+using namespace RhIO;
+using namespace RhAL;
 
+RhioBinding::RhioBinding(BaseManager *manager_, std::string node_)
+    : thread(NULL), manager(manager_), bind(node_)
+{
     // Binding methods
-    node->bindFunc("scan", "Force the scan",
+    bind.bindFunc("scan", "Force the scan",
             &RhioBinding::cmdScan, *this);
+    bind.bindFunc("readDev", "Read all registers from a device",
+            &RhioBinding::cmdReadDev, *this, {"unknown"});
 
     // Starts the update
     update();
 }
 
+RhioBinding::~RhioBinding()
+{
+    if (thread) {
+        over = true;
+        thread->join();
+        delete thread;
+    }
+}
+
+void RhioBinding::runScheduler()
+{
+    // Runs the scheduler
+    manager->setScheduleMode(true);
+    thread = new std::thread([this] {
+        while (!this->over) {
+            manager->flush();
+            update();
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        }
+    });
+}
+
+std::vector<Device*> RhioBinding::getDevices()
+{
+    std::vector<Device*> devices;
+    auto all = manager->devContainer();
+
+    for (auto entry : all) {
+        devices.push_back(entry.second);
+    }
+
+    return devices;
+}
+
 void RhioBinding::update()
 {
-    auto allDevices = manager->getDevices();
+    auto allDevices = getDevices();
 
     for (auto device : allDevices) {
         // The device doesn't have a corresponding RhIO node, let's
         // create it and populate it
+        auto node = &bind.node();
         if (!node->childExist(device->name())) {
             node->newChild(device->name());
         }
-        auto deviceNode = node->getChild(device->name());
+        auto deviceNode = &node->child(device->name());
 
         // Iterating over all registers
         for (const auto &entry : device->registersList().container()) {
@@ -35,44 +71,50 @@ void RhioBinding::update()
             auto reg = entry.second;
 
             if (deviceNode->getValueType(name) == NoValue) {
+                TypedRegisterBool *registerBool;
+                TypedRegisterInt *registerInt;
+                TypedRegisterFloat *registerFloat;
                 // Setup the node and corresponding callbacks
-                if (auto registerBool = dynamic_cast<TypedRegisterBool*>(reg)) {
-                    deviceNode->newBool(name, registerBool.readValue().value);
-                    deviceNode->setCallbackBool(name, [&registerBool](bool newValue) {
-                        registerBool->writeValue(newValue);
+                if ((registerBool = dynamic_cast<TypedRegisterBool*>(reg)) != NULL) {
+                    deviceNode->newBool(name);
+                    deviceNode->setBool(name, registerBool->readValue().value);
+                    deviceNode->setCallbackBool(name, [registerBool](bool newValue) {
+                        registerBool->writeValue(newValue, true);
                     });
                     auto callback = [deviceNode, name](bool newValue) {
-                        deviceNode->setBool(name, newValue);
+                        deviceNode->setBool(name, newValue, true);
                     };
-                    registerBool->addReadCallback(callback);
-                    registerBool->addWriteCallback(callback);
-                } else if (auto registerInt = dynamic_cast<TypedRegisterBool*>(reg)) {
-                    deviceNode->newInt(name, registerBool.readValue().value);
-                    deviceNode->setCallbackInt(name, [&registerInt](int newValue) {
-                        registerInt->writeValue(newValue);
+                    registerBool->setCallbackRead(callback);
+                    registerBool->setCallbackWrite(callback);
+                } else if ((registerInt = dynamic_cast<TypedRegisterInt*>(reg)) != NULL) {
+                    deviceNode->newInt(name);
+                    deviceNode->setInt(name, registerInt->readValue().value);
+                    deviceNode->setCallbackInt(name, [registerInt](int newValue) {
+                        registerInt->writeValue(newValue, true);
                     });
                     auto callback = [deviceNode, name](int newValue) {
-                        deviceNode->setInt(name, newValue);
+                        deviceNode->setInt(name, newValue, true);
                     };
-                    registerInt->addReadCallback(callback);
-                    registerInt->addWriteCallback(callback);
-                } else if (auto registerFloat = dynamic_cast<TypedRegisterFloat*>(reg)) {
-                    deviceNode->newFloat(name, registerBool.readValue().value);
-                    deviceNode->setCallbackFloat(name, [&registerFloat](float newValue) {
-                        registerFloat->writeValue(newValue);
+                    registerInt->setCallbackRead(callback);
+                    registerInt->setCallbackWrite(callback);
+                } else if ((registerFloat = dynamic_cast<TypedRegisterFloat*>(reg)) != NULL) {
+                    deviceNode->newFloat(name);
+                    deviceNode->setFloat(name, registerFloat->readValue().value);
+                    deviceNode->setCallbackFloat(name, [registerFloat](float newValue) {
+                        registerFloat->writeValue(newValue, true);
                     });
                     auto callback = [deviceNode, name](float newValue) {
-                        deviceNode->setFloat(name, newValue);
+                        deviceNode->setFloat(name, newValue, true);
                     };
-                    registerFloat->addReadCallback(callback);
-                    registerFloat->addWriteCallback(callback);
+                    registerFloat->setCallbackRead(callback);
+                    registerFloat->setCallbackWrite(callback);
                 }
             }
        
             if (!deviceNode->childExist("parameters")) {
                 deviceNode->newChild("parameters");
             }
-            auto parametersNode = deviceNode->getChild("parameters");
+            auto parametersNode = &deviceNode->child("parameters");
 
             // Iterating over all parameters and exporting to RhIO
             for (const auto &entry : device->parametersList().containerBool()) {
@@ -80,38 +122,41 @@ void RhioBinding::update()
                 auto parameter = entry.second;
 
                 if (parametersNode->getValueType(name) == NoValue) {
-                    parametersNode->newBool(name, parameter.value);
-                    parametersNode->addCallback([parameter](bool newValue) {
-                        parameter.value = newValue;
+                    parametersNode->newBool(name);
+                    parametersNode->setBool(name, parameter->value);
+                    parametersNode->setCallbackBool(name, [parameter](bool newValue) {
+                        parameter->value = newValue;
                     });
                 } else {
-                    parametersNode->setBool(name, parameter.value);
+                    parametersNode->setBool(name, parameter->value);
                 }
             }
-            for (const auto &entry : device->parametersList().containerInt()) {
+            for (const auto &entry : device->parametersList().containerStr()) {
                 auto name = entry.first;
                 auto parameter = entry.second;
 
                 if (parametersNode->getValueType(name) == NoValue) {
-                    parametersNode->newInt(name, parameter.value);
-                    parametersNode->addCallback([parameter](int newValue) {
-                        parameter.value = newValue;
+                    parametersNode->newStr(name);
+                    parametersNode->setStr(name, parameter->value);
+                    parametersNode->setCallbackStr(name, [parameter](std::string newValue) {
+                        parameter->value = newValue;
                     });
                 } else {
-                    parametersNode->setInt(name, parameter.value);
+                    parametersNode->setStr(name, parameter->value);
                 }
             }
-            for (const auto &entry : device->parametersList().containerFloat()) {
+            for (const auto &entry : device->parametersList().containerNumber()) {
                 auto name = entry.first;
                 auto parameter = entry.second;
 
                 if (parametersNode->getValueType(name) == NoValue) {
-                    parametersNode->newFloat(name, parameter.value);
-                    parametersNode->addCallback([parameter](float newValue) {
-                        parameter.value = newValue;
+                    parametersNode->newFloat(name);
+                    parametersNode->setFloat(name, parameter->value);
+                    parametersNode->setCallbackFloat(name, [parameter](float newValue) {
+                        parameter->value = newValue;
                     });
                 } else {
-                    parametersNode->setFloat(name, parameter.value);
+                    parametersNode->setFloat(name, parameter->value, false);
                 }
             }
         }
@@ -120,8 +165,21 @@ void RhioBinding::update()
 
 std::string RhioBinding::cmdScan()
 {
-    manager.scan();
+    manager->scan();
     update();
 
     return "Running a scan";
+}
+            
+std::string RhioBinding::cmdReadDev(std::string name)
+{
+    auto &device = manager->devByName(name);
+    for (const auto &entry : device.registersList().container()) {
+        entry.second->forceRead();
+    }
+    update();
+
+    std::stringstream ss;
+    ss << "Reading all registers from device " << name;
+    return ss.str();
 }
